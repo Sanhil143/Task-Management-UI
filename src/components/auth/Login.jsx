a92ed6090b73c5cb8bb97338682bb2786ce3a7cf
Sanhil143 <sanhilrai143@gmail.com>
2024-04-13 03:38:20 +0530

    setup with login page

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,116 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const Login = () => {
+  const [formdata, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/auth/login",
+        formdata
+      );
+      if (response.data) {
+        localStorage.setItem("accessToken", response.data.token);
+        localStorage.setItem("userId", response.data.userId);
+        localStorage.setItem("userType", response.data.userType);
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formdata, [name]: value });
+  };
+  
+  //variable css
+  const mainDiv = {
+    display: "flex",
+    width: "100%",
+    height: "100vh",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "5px solid orange",
+    boxSizing: "border-box",
+  };
+  const formStyle = {
+    width: "25%",
+    height: "400px",
+    textAlign: "center",
+    border: "1px solid black",
+    boxShadow: "1px 1px grey",
+  };
+  const fieldBar = {
+    marginTop: "20px",
+  };
+  const loginContainer = {
+    position: "relative",
+    top: "90px",
+  };
+  const formLabel = {
+    display: "inline-block",
+    width: "60px",
+    marginRight: "25px",
+    textAlign: "left",
+  };
+  const formInput = {
+    padding: "10px",
+    border: "1px solid #ccc",
+    borderRadius: "15px",
+  };
+  const buttonInput = {
+    marginTop: "20px",
+    padding: "8px 60px",
+    borderRadius: "15px",
+    cursor: "pointer",
+  };
+  return (
+    <div style={mainDiv}>
+      <form style={formStyle} onSubmit={handleSubmit}>
+        <div style={loginContainer}>
+          <div style={fieldBar}>
+            <label style={formLabel} htmlFor="email">
+              email
+            </label>
+            <input
+              style={formInput}
+              id="email"
+              name="email"
+              type="text"
+              placeholder="email"
+              value={formdata.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div style={fieldBar}>
+            <label style={formLabel} htmlFor="password">
+              password
+            </label>
+            <input
+              style={formInput}
+              id="password"
+              name="password"
+              type="password"
+              placeholder="password"
+              value={formdata.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button style={buttonInput} type="submit">
+            login
+          </button>
+          <p>create your new Journey with us </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
